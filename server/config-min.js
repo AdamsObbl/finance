4aec6df945f570d1ab724da04e50f42fa0604d63
Adam Szczepinski <adams.szczepinski@gmail.com>
2024-03-28 18:41:27 +0100

    fix

--- server/config-min.js
+++
@@ -1,1 +0,0 @@
-const config={db:{host:"mysql.mikr.us",user:"u272",password:"BE2B_7e02e0",database:"db_u272",connectTimeout:6e4},listPerPage:10};module.exports=config;
--- server/services/db-min.js
+++
@@ -1,1 +0,0 @@
-const mysql=require("mysql2/promise"),config=require("../config");async function query(e,n){const r=await mysql.createConnection(config.db),[o]=await r.execute(e,n);return o}module.exports={query:query};
